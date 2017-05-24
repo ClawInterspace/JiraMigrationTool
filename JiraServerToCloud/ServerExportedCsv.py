edf96a69032c01f181bf0e531cc35944146fb334
Alan Liu (劉承易) <alanliu@cybersoft4u.com>
2017-05-24 18:57:40 +0800

    extract attach info done with unittest

--- JiraServerToCloud/ServerExportedCsv.py
+++ JiraServerToCloud/ServerExportedCsv.py
@@ -15,24 +15,95 @@ def remap(fieldnames, duplicated_field_name):
 
 class Reader(object):
 
-    issue_fname_lookup_map = {}
+    issue_attach_info = {}
+    # {old_issue_key: IssueAttachmentMigrationInfo}
+
     issueid_attachments_map = {}
+    # {filepath: (old_filename, new_filename, old_issue_key)}
 
     def __init__(self, file_path):
         """
 
         :param file_path:
         """
+        self.issue_attach_info = {}
+        self.issueid_attachments_map = {}
+
         self._file_path = file_path
 
-    def build_lookup_map(self):
+    def build_attach_name_info(self):
 
         with open(self._file_path, 'rb') as csvfile:
+
             reader = csv.reader(csvfile)
-            fieldnames = remap(next(reader), 'Attachment')
+
+            # fn: field name
+            fn_issue_key = 'Issue key'
+            fn_attachment = 'Attachment'
+
+            # rename field name to avoid duplicated field(attachment) name
+            fieldnames = remap(next(reader), fn_attachment)
+
             for row in reader:
+
                 row = dict(zip(fieldnames, row))
-                print row['Issue key'], \
-                    row['Attachment1'], \
-                    row['Attachment2'], \
-                    row['Attachment3']
+
+                tmp_issue = IssueAttachmentMigrationInfo()
+                tmp_issue.old_issue_key = row[fn_issue_key]
+
+                _logger.debug(row[fn_issue_key])
+
+                for field_name, field_value in row.iteritems():
+                    if field_name.startswith(fn_attachment) and field_value.strip() != "":
+                        file_id, file_name = \
+                            self._extract_attach_info_from_csv_value(field_value)
+                        tmp_issue.attach_info.append((file_id, file_name))
+
+                if len(tmp_issue.attach_info) > 0:
+                    self.issue_attach_info[tmp_issue.old_issue_key] = tmp_issue
+
+    @classmethod
+    def _extract_attach_info_from_csv_value(cls, cell_value):
+        """
+        
+        :param cell_value:
+         example value, "19/May/17 5:33 PM;alanliu;jira-migration-drill-a.png;http://192.168.2.222/secure/attachment/10100/jira-migration-drill-a.png"
+        :return: 
+        """
+        segments = cell_value.split(';')
+
+        try:
+            # TODO: more specific format checking
+            date = segments[0]
+            user_name = segments[1]
+            file_name = segments[2]
+            file_url = segments[3]
+            file_id = file_url.strip().split('/')[-2]
+
+        except Exception as e:
+            raise Exception('cell value format error: "%s"' % cell_value)
+
+        return file_id, file_name
+
+
+class IssueAttachmentMigrationInfo(object):
+
+    CLOUD_BASE_URL = ''
+    CLOUD_USER_NAME = ''
+    CLOUD_PASSWORD = ''
+
+    old_issue_key = ''
+    # an issue perhaps has more than one attachment
+    # [(file_id1, file_name1), (file_id2, file_name2)...]
+    attach_info = []
+
+    file_base_path = ''
+    new_issue_key = ''
+
+    def __init__(self):
+        self.attach_info = []
+
+    def display_all_attach_info(self):
+        print self.old_issue_key
+        for attach_info in self.attach_info:
+            print attach_info[0], attach_info[1]
--- test/JiraServerToCloudTest/testCsvReader.py
+++ test/JiraServerToCloudTest/testCsvReader.py
@@ -1,3 +1,4 @@
+# coding: utf-8
 import unittest
 
 from JiraServerToCloud.ServerExportedCsv import Reader
@@ -6,11 +7,67 @@ from JiraServerToCloud.ServerExportedCsv import Reader
 class testCsvReader(unittest.TestCase):
 
     def setUp(self):
-        self.test_file = 'Cybersoft-Migration.csv'
+        pass
 
-    def test_something(self):
-        csv_reader = Reader(self.test_file)
-        csv_reader.build_lookup_map()
+    def test_en_csv_file(self):
+
+        test_file = 'Cybersoft-Migration.csv'
+
+        csv_reader = Reader(test_file)
+        csv_reader.build_attach_name_info()
+
+        for issue, info in csv_reader.issue_attach_info.iteritems():
+            print issue
+            print len(info.attach_info)
+            info.display_all_attach_info()
+
+        self.assertEquals(
+            len(csv_reader.issue_attach_info.get('MIG-3').attach_info),
+            1)
+
+        self.assertIn(
+            ('10100', 'jira-migration-drill-a.png'),
+            csv_reader.issue_attach_info.get('MIG-3').attach_info)
+
+        self.assertEquals(
+            len(csv_reader.issue_attach_info.get('MIG-1').attach_info),
+            3)
+
+        self.assertIn(
+            ('10101', 'jira-migration-drill-a.png'),
+            csv_reader.issue_attach_info.get('MIG-1').attach_info)
+
+        self.assertIn(
+            ('10000', '[2016-09-11] 01_48_59.png'),
+            csv_reader.issue_attach_info.get('MIG-1').attach_info)
+
+        self.assertIn(
+            ('10001', '[2015-10-24] 00_08_29.png'),
+            csv_reader.issue_attach_info.get('MIG-1').attach_info)
+
+        self.assertIsNone(csv_reader.issue_attach_info.get('MIG-2'))
+
+    def test_tw_csv_file(self):
+
+        test_file = 'Cybersoft-Migration_中文.csv'
+
+        csv_reader = Reader(test_file)
+        csv_reader.build_attach_name_info()
+
+        for issue, info in csv_reader.issue_attach_info.iteritems():
+            print issue
+            print len(info.attach_info)
+            info.display_all_attach_info()
+
+        self.assertEquals(
+            len(csv_reader.issue_attach_info.get('MIG-2').attach_info),
+            2)
+
+        self.assertIn(
+            ('10201', '螢幕快照 2013-12-05 下午10.16.24.jpg'),
+            csv_reader.issue_attach_info.get('MIG-2').attach_info)
+
+
 
 if __name__ == '__main__':
     unittest.main()
